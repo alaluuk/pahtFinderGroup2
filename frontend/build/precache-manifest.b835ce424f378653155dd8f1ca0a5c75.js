a6047541a559175886cb01db10ce4fca961438ee
Tim Pachmann <tim.pachmann@stud.h-da.de>
2019-11-04 17:49:26 +0200

    Repository migration
    Migrated to the new repository, added basic NodeJS server (with Express & GraphQL), added CI/CD pipeline

---
+++ frontend/build/precache-manifest.b835ce424f378653155dd8f1ca0a5c75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccc1d787ec70e1d9bc0eb77a1cadc31b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f31812c340f88b232a1d",
+    "url": "/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "c5f2f375fd0d91942762",
+    "url": "/static/js/2.2b5ccbfc.chunk.js"
+  },
+  {
+    "revision": "f31812c340f88b232a1d",
+    "url": "/static/js/main.6608d880.chunk.js"
+  },
+  {
+    "revision": "113e7ca9ccaa29a68c40",
+    "url": "/static/js/runtime-main.22228283.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
