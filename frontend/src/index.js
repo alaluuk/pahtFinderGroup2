e92fa3b9cc2e91abb78562cc3a59a21e2749ac09
jakob <jakob.benkoe@gmail.com>
2019-11-12 00:08:22 +0200

    router comp & apollo

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
-import Overview from './pages/overview';
-import Landing from './pages/landing';
-import AddBuilding from './pages/addBuilding';
+import CustomRouter from './components/CustomRouter';
 import * as serviceWorker from './serviceWorker';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+const client = new ApolloClient({
+  uri: 'https://oamk-pathfinder.herokuapp.com/graphql'
+});
 
 
-const routing = (
-    <Router>
-      <div>
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/overview" component={Overview} />
-        <Route exact path ="/addbuilding" component={AddBuilding}/>
-      </div>
-    </Router>
-  )
-
-ReactDOM.render(routing, document.getElementById('root'));
+ReactDOM.render(
+  <CustomRouter>
+  <ApolloProvider client={client}><routing/></ApolloProvider>
+  </CustomRouter>
+  ,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
