72266139c5932ce463f072da7450290270d01c4e
Simon Janik <sj043@hdm-stuttgart.de>
2019-11-13 13:57:34 +0200

    Changes frontend

--- frontend/src/components/Overview/body.jsx
+++ frontend/src/components/Overview/body.jsx
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import Card from '../card';
+import AddCard from '../addCard';
 
 class Body extends Component {
     state = {  }
     render() { 
         return ( <div className = "body">
                     <div className = "content">
-                        <h1>Overview</h1>
+                        <h1 className = "overviewHeader">My buildings</h1>
+                        <div className = "scrollBars">
                         <Card></Card>
+                        <Card></Card>
+                        <Card></Card>
+                        <AddCard></AddCard>
+                        </div>
+
+                        <div className = "bottomContent">
+                            <h1>hello</h1>
+                            <h1>hello</h1>
+
+                        </div>
+                        
                     </div>
         
                 </div> );
--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,20 +1,47 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom';
+import Overview from '../pages/overview';
+import Link from 'react-router-dom';
 
 
 class Header extends Component {
     state = {  }
+
+    loadOverview = () => {
+       ReactDOM.render(Overview, document.getElementById('root'));
+      }
+
+    loadMarketplace = () => {
+    console.log('this is:', this);
+    }
+
+    loadShop = () => {
+        console.log('this is:', this);
+        }
+
     render() { 
         return (
 
         <div className = "header">
 
-            <div className = "landingHeader"> 
+            <h1 className = "header_logo">greenHouse</h1>
+
+            <div className = "menu">
+            
+                <p className = "menuItem" onClick={this.loadOverview}>My buildings</p>
+                <p className = "menuItem" onClick={this.loadMarketplace} >Marketplace</p>
+                <p className = "menuItem" onClick={this.loadShop}>Shop</p>
+            </div>
+
+            <div className = "login"> 
             <img className = "loginLogo" src = "https://www.svgrepo.com/show/81219/user-symbol-of-thin-outline.svg"></img>
 
+            
             <h3 className = "loginText">Sign In</h3>
             </div>
 
-            <h1 className = "header_logo">greenHouse</h1>
+            
+            
             
             
 
